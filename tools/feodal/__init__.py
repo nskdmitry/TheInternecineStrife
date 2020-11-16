e1579a2a7b59421b57b85130d83981dac9eb6e5e
nskdmitry <dmitry.m-ko2014@yandex.ru>
2020-11-16 19:27:28 +0700

    Add a comment of package.

--- tools/feodal/__init__.py
+++ tools/feodal/__init__.py
@@ -1,2 +1,1 @@
-""" All function for work with The Feodal World Engine maps, games, patterns and files. """
 __all__ = ["feods", "generators", "MapBackgroundGenerator", "constants", "Map", "MapTemplate", "tools", "typical", "lands", "environments", "pathfinding", "pngs"]
