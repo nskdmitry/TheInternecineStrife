d015925f42b324c299ed72a391236fc0935b7c0d
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-11-01 12:53:24 +0700

    Remove unused 'using ' constructions.

--- src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/War/Weapon.cs
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/War/Weapon.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace TheInternecineStrife.ServerSide.Model.War
+﻿namespace TheInternecineStrife.ServerSide.Model.War
 {
     public class Weapon
     {
