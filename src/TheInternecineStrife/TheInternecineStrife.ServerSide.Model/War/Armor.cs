a8ef9b5796b78cdfa92bf038116d3259538de011
nskdmitry <dmitry.m-ko2014@yandex.ru>
2021-11-01 11:43:34 +0700

    New basic classes of Warfare: Armor (make chances to ignore full damage) and Division (squad from vassales what constucts armies).

---
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/War/Armor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheInternecineStrife.ServerSide.Model.War
+{
+    public class Armor
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public float DefenceLevel {
+            get { return _surface_closed; }
+            set { _surface_closed = Math.Max(0.00f, Math.Min(value, 1.00f)); }
+        }
+        public int Resource { get; protected set; }
+
+        private float _surface_closed = 0;
+    }
+}
---
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/War/Division.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace TheInternecineStrife.ServerSide.Model.War
+{
+    public enum BattleSide { Left = -1, Center = 0, Right = 1 };
+
+    /// <summary>
+    /// Полк в составе армии. Может принадлежать кому угодно, не только тому, кто сформировал армию. Воевать может только в составе армии.
+    /// </summary>
+    public class Division : Protocol.Controllable
+    {
+        public SoldierProfile Profile { get; protected set; } // Тип войск
+        public int Strength { get; set; } = 0; // Численость
+        public float Energy { get; protected set; } = 1; // Силы сражаться
+        public Battle.Formation Formation { get; set; } // Строй
+        public bool Regular { get; set; } = false; // Расформировать по достижению NextPayDay
+        public int NextPayDay { get; set; } // Дата получки или авторасформирования войск.
+        public Cell Sender { get; protected set; } // Кто прислал войска, кто им платит и кому они на самом деле верны
+        public BattleSide FrontWing { get; set; } = BattleSide.Center; // Крыло
+
+        public Division(string title, int gameDay, Cell coordinator, SoldierProfile warclass, bool regular = false)
+        {
+            Name = title;
+            Sender = coordinator;
+            Owner = coordinator.Settling.Owner;
+            Strength = 0;
+            Profile = warclass;
+            Energy = 1.00f;
+            Formation = new Battle.Formation() {
+                Columns = 10,
+                Rows = 20,
+                Density = 3f,
+                FireOnMarsh = false,
+                MaleeAttackDistance = warclass.Malee.Far,
+                MaleeResource = warclass.Malee?.Resource ?? 0,
+                RetreetDistance = -20*3f,
+                RangeAttackDistance = warclass.Range?.Far-2 ?? 0f,
+                RangeResource = warclass.Range?.Resource ?? 0,
+                Ranger = warclass.Range != null,
+                NoBlendMalee = true,
+                Woundeds = new List<int>(),
+                Medics = 0
+            };
+            Regular = regular;
+            NextPayDay = gameDay + 5;
+        }
+    }
+}
