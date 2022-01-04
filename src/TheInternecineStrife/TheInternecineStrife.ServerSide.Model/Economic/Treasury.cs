5522bd97a82f15fe9d0ecea36647c5489fc2df18
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-04 20:05:44 +0700

    features(Treasury) New operator Treasury -(Treasury). New method Treasury Withdraw(Treasury price). New method bool FasableIncome(Treasury income).

--- src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Economic/Treasury.cs
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Economic/Treasury.cs
@@ -6,85 +6,84 @@ using System.Linq;
 
 namespace TheInternecineStrife.ServerSide.Model.Economic
 {
-    public struct GoodItem
-    {
-        public Good  Type;
-        public float Amount;
-        public float Cost
-        {
-            get
-            {
-                return Type.Price * Amount;
-            }
-        }
-    }
+    public enum AllProduction { None, Food, Cattle, Fuel, Gold, Source, Catridges, Weapon, SeigeTechnic };
 
     public class Treasury
     {
-    	public float Gold;
-    	public float Supplies;
-    	public float Raw;
-    	public int Weapons;
-    	
-    	public int Slaves;
+        public int Id { get; set; }
         /// <summary>
-        /// Наёмники
+        /// Поддерживается механизм долга:)
         /// </summary>
-        public List<GoodItem> Mercenaries = new List<GoodItem>(10);
-
-        public const double BUY_KOEFFICIENT = 1;
-        public const double MERCENARY_COEFFICIENT = 3.5;
+        public float Gold { get; set; } = 0;
+        public float Food { get => _food; set => _food = value >= 0 ? value : 0; }
+        public float Fuel { get => _oil; set => _oil = value >= 0 ? value : 0; }
+        public float Cattle { get => (float)_rams; set => _rams = value >= 0 ? (int)value : 0; }
+        public float Catridges { get => _arrows; set => _arrows = value >= 0 ? value : 0; }
+        public float WeaponArmor { get => _equip; set => _equip = value >= 0 ? value : 0; }
+        public float SourceMaterials { get => _sources; set => _sources = value >= 0 ? value : 0; }
         
-        public static Treasury operator +(Treasury accumulator, Treasury goods)
+        public static Treasury operator +(Treasury stock, Treasury source)
         {
-        	var sum = new Treasury() {
-        		Gold = accumulator.Gold + goods.Gold,
-        		Supplies = accumulator.Supplies + goods.Supplies,
-        		Raw = accumulator.Raw + goods.Raw,
-        		Weapons = accumulator.Weapons + goods.Weapons,
-        	};
-        	sum.Slaves = accumulator.Slaves + goods.Slaves;
-        	sum.Mercenaries.AddRange(accumulator.Mercenaries);
-        	sum.Mercenaries.AddRange(goods.Mercenaries);
-        	
-            return sum;
+            stock.Gold += source.Gold;
+            stock.Food += source.Food;
+            stock.Fuel += source.Fuel;
+            stock.Cattle += source.Cattle;
+            stock.Catridges += source.Catridges;
+            stock.WeaponArmor += source.WeaponArmor;
+            stock.SourceMaterials += stock.SourceMaterials;
+            return stock;
         }
 
-        public static Treasury operator *(Treasury source, float koefficient)
+        public static Treasury operator-(Treasury price)
         {
-            return new Treasury {
-                Gold = koefficient * source.Gold,
-                Supplies = koefficient * source.Supplies,
-                Raw = koefficient * source.Raw,
-                Weapons = (int) koefficient * source.Weapons,
-                Slaves = (int) koefficient * source.Slaves,
-                Mercenaries = source.Mercenaries
+            return new Treasury
+            {
+                Catridges = -price.Catridges,
+                Cattle = -price.Cattle,
+                Gold = -price.Gold,
+                Food = -price.Food,
+                Fuel = -price.Fuel,
+                SourceMaterials = -price.SourceMaterials,
+                WeaponArmor = -price.WeaponArmor,
             };
         }
 
-        public static Treasury operator -(Treasury from, Treasury to)
+        public Treasury Withdraw(Treasury resources)
         {
-            from.Gold -= to.Gold;
-            from.Raw -= to.Raw;
-            from.Slaves -= to.Slaves;
-            from.Supplies -= to.Supplies;
-            from.Weapons -= to.Weapons;
-            return from;
+            resources.Catridges = Math.Min(resources.Catridges, Catridges);
+            resources.Cattle = Math.Min(resources.Cattle, Cattle);
+            resources.Gold = Math.Min(resources.Gold, Gold);
+            resources.Food = Math.Min(resources.Food, Food);
+            resources.Fuel = Math.Min(resources.Fuel, Fuel);
+            resources.SourceMaterials = Math.Min(resources.SourceMaterials, SourceMaterials);
+            resources.WeaponArmor = Math.Min(resources.WeaponArmor, WeaponArmor);
+
+            Catridges -= resources.Catridges;
+            Cattle -= resources.Cattle;
+            Gold -= resources.Gold;
+            Food -= resources.Food;
+            Fuel -= resources.Fuel;
+            SourceMaterials -= resources.SourceMaterials;
+            WeaponArmor -= resources.WeaponArmor;
+
+            return resources;
         }
 
-        internal void Flush()
+        public virtual float Product(float instruments)
         {
-            Gold = 0;
-            Supplies = 0;
-            Raw = 0;
-            Weapons = 0;
-            Slaves = 0;
-            Mercenaries.Clear();
+            return 0;
         }
 
-        public override string ToString()
+        protected virtual bool FeasableIncome(Treasury income)
         {
-            return Gold.ToString("0.00");
+            return true;
         }
+
+        protected float _food = 0;
+        protected float _arrows = 0;
+        protected float _equip = 0;
+        protected float _sources = 0;
+        protected float _oil = 0;
+        protected int _rams = 0;
     }
 }
