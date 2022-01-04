af0f2b811fc995b2f2034bc380c0468f29e16744
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-04 21:51:50 +0700

    refactor(warfare)
    Division linked to Contract, Stratum and Place (Home). Division has own wagoon with baggage too.

--- src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/War/Division.cs
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/War/Division.cs
@@ -1,28 +1,90 @@
 ﻿using System.Collections.Generic;
+using TheInternecineStrife.ServerSide.Model.Protocol;
 
 namespace TheInternecineStrife.ServerSide.Model.War
 {
     public enum BattleSide { Left = -1, Center = 0, Right = 1 };
+    public enum BattleDepth { Avan = -1, Body = 0, Arier = 2 }; 
+    public enum ContractClass { Military, Mercenary, Regulary };
+    public enum StrongNominal { Subdivision=10, Hundred=100, Thousand=1000, Legion=10000 };
+
+    /// <summary>
+    /// Контракт, описывающий условия службы
+    /// </summary>
+    public struct Contract
+    {
+        public ContractClass Class { get; set; }
+        public int Day { get; set; }
+        public int Length { get; }
+        public int Deadline { get; private set; }
+        // Закончилось ли формирование отряда? 
+        // Если да, его уже не пополнишь. Если не сформирован, то его не отправишь.
+        public bool Formed
+        {
+            get { return _formed; }
+            set { if (!_formed) _formed = value; }
+        }
+        /// <summary>
+        /// Номинальная численность.
+        /// По ней считаются потребности провизии, оружии. По ней же считается зарплата.
+        /// Также именно по ним считается "наглядная" численность войск.
+        /// </summary>
+        public StrongNominal Nominal { get; set; }
+
+        public Contract(ContractClass relation, StrongNominal size, int now, int contractDays)
+        {
+            Class = relation;
+            Day = now;
+            Length = contractDays;
+            Deadline = now + contractDays;
+            Nominal = size;
+            _formed = false;
+        }
+
+        public void Prolong()
+        {
+            if (_formed) Deadline += Length;
+        }
+
+        private bool _formed;
+    }
 
     /// <summary>
     /// Полк в составе армии. Может принадлежать кому угодно, не только тому, кто сформировал армию. Воевать может только в составе армии.
     /// </summary>
-    public class Division : Protocol.Controllable
+    public class Division : Controllable
     {
         public SoldierProfile Profile { get; protected set; } // Тип войск
+        public Social.StratumClass Stracia { get; set; } // Могут собираться только из равных
         public int Strength { get; set; } = 0; // Численость
-        public float Energy { get; protected set; } = 1; // Силы сражаться
-        public Battle.Formation Formation { get; set; } // Строй
-        public bool Regular { get; set; } = false; // Расформировать по достижению NextPayDay
-        public int NextPayDay { get; set; } // Дата получки или авторасформирования войск.
-        public Cell Sender { get; protected set; } // Кто прислал войска, кто им платит и кому они на самом деле верны
-        public BattleSide FrontWing { get; set; } = BattleSide.Center; // Крыло
+        public Contract Contract { get; set; } // Контракт, по которому дивизия и несёт службу
+        public Controllable Home { get; protected set; } // Кто прислал войска, кто им платит и кому они на самом деле верны
+        public Economic.Wagoon Baggage { get; } // Обоз
 
-        public Division(string title, int gameDay, Cell coordinator, SoldierProfile warclass, bool regular = false)
+        /// <summary>
+        /// Боевые качества
+        /// </summary>
+        public float Energy { get; set; } = 1; // Силы сражаться
+        public Battle.Formation Formation { get; set; } // Строй
+        public float Experience { get => exp; set { if (value > exp) exp = value; } }
+
+        /// <summary>
+        /// Положение отряда при развёртывании перед боем.
+        /// </summary>
+        public BattleSide FrontWing { get; set; } = BattleSide.Center; // Крыло
+        public BattleDepth FrontDepth { get; set; }
+
+        public Division(
+            string title, 
+            int gameDay, 
+            Controllable coordinator, 
+            SoldierProfile warclass, 
+            ContractClass contract,
+            StrongNominal force)
         {
             Name = title;
-            Sender = coordinator;
-            Owner = coordinator.Settling.Owner;
+            Home = coordinator;
+            Owner = coordinator.Owner;
             Strength = 0;
             Profile = warclass;
             Energy = 1.00f;
@@ -37,12 +99,20 @@ namespace TheInternecineStrife.ServerSide.Model.War
                 RangeAttackDistance = warclass.Range?.Far-2 ?? 0f,
                 RangeResource = warclass.Range?.Resource ?? 0,
                 Ranger = warclass.Range != null,
-                NoBlendMalee = true,
-                Woundeds = new List<int>(),
-                Medics = 0
+                NoBlendMalee = true
             };
-            Regular = regular;
-            NextPayDay = gameDay + 5;
+            Contract = new Contract(contract, force, gameDay, 5);
+            Baggage = new Economic.Wagoon()
+            {
+                Private = this,
+            };
         }
+
+        public float MaleePercision { get => 0.85f - 1/(exp + 2); }
+        public float RangePercision { get => 1 - 1/(exp + 2); }
+        public float OrderDeliverSpeed { get => 1 - 1000/(exp + 1000); }
+        public float FormationUnoSpeed { get => OrderDeliverSpeed; }
+
+        private float exp = 0;
     }
 }
