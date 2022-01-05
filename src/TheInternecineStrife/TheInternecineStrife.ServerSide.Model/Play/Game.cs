da861b028e7f8aea16579eded21af03a8ac488c5
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-05 18:49:44 +0700

    feature(Game playing) New class Game.
    Game contains a static, constant data of play game: name, map, creation date, author and another.

---
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Play/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheInternecineStrife.ServerSide.Model.Play
+{
+    public class Game
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public GameFrame Status { get; set; }
+        //public War.Army LastActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
