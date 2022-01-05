050e4e498609ee4cb3966538ba02faac86711cdd
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-05 19:59:34 +0700

    feature(economic) New interface IEconomicable.
    IEconomicable unites treasury, production of gold and another, order for workers and tenders frrom they for resources.

---
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Protocol/IEconomicable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheInternecineStrife.ServerSide.Model.Economic;
+
+namespace TheInternecineStrife.ServerSide.Model.Protocol
+{
+    interface IEconomicable
+    {
+        Treasury Economic { get; set; }
+        float Product(int day, float instrument);
+        Social.CraftOrder Order { get; set; }
+        List<Social.CraftOrder> Tenders { get; }
+    }
+}
