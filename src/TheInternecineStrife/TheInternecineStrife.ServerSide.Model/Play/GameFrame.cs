ebdaadbf2cffb0161194a341182bcd71221268e0
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-05 18:45:39 +0700

    feature(Game playing) New class GameFrame.
    GameFrame is dynamical, state-content, storable and constantable part of Game. It contains updateable properties of Game.

---
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Play/GameFrame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheInternecineStrife.ServerSide.Model.Play
+{
+    public class GameFrame
+    {
+        public uint Id { get; set; }
+        public uint GameId { get; set; }
+        public World.Map Playground { get; set; }
+        public DateTime Updated { get; set; }
+        public int PlayholderId { get; set; } // Игрок, сделавший ход
+        public int CircleNo { get; set; }
+        public List<DivisionIncoming> DivisionsInRun { get; } = new List<DivisionIncoming>(10);
+    }
+}
