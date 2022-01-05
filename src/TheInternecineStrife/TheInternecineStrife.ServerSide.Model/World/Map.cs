754c694b2a2267e1442239f14dd03e7fcd1308b3
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-05 20:31:55 +0700

    feature(game playing) New class Map.
    Map accumulates a data about of land (Ground), politic areas (Domains), settlements (Dwellings) and war contingents (Armies) and some another.

---
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/World/Map.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheInternecineStrife.ServerSide.Model.World
+{
+    public class Map
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public uint Face { get; set; }
+        public Cell[] Ground { get; set; }
+        public List<Domain> Domains { get; set; }
+        public List<Dwelling> Dwellings { get; set; }
+        public List<War.Army> Armies { get; set; }
+
+        /*
+         * None-playable metainformation.
+         */
+        public string GeneratorName { get; set; }
+        public DateTime Created { get; set; }
+        public string Author { get; set; }
+
+        public uint GetCellIndex(uint x, uint y) => x + y * Face;
+    }
+}
