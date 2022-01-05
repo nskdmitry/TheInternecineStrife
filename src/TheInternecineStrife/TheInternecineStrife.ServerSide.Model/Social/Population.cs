0c3b43d7402c1a6e3d45ac38fc2b790e41fe510e
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-05 19:49:40 +0700

    refactor(population) Restructure Population.Death(man, feman, kids) and Population.DistributionFunds(golds) methods.
    Separate stages of functions to the functions.

--- src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Social/Population.cs
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Social/Population.cs
@@ -22,7 +22,9 @@ namespace TheInternecineStrife.ServerSide.Model.Social
         public Stratum Serfs = new Stratum(StratumClass.Classes[5]);
         public Stratum Slaves = new Stratum(StratumClass.Classes[6]);
 
-        public int Men { get { return Nobility.Man + Merchantes.Man + ArtistCraft.Man + Clir.Man + Freeman.Man + Serfs.Man; } }
+        public int Men {
+            get { return Nobility.Man + Merchantes.Man + ArtistCraft.Man + Clir.Man + Freeman.Man + Serfs.Man; }
+        }
         public int Femen {
             get { return Nobility.Feman + Merchantes.Feman + ArtistCraft.Feman + Clir.Feman + Freeman.Feman + Serfs.Feman; }
         }
@@ -30,9 +32,10 @@ namespace TheInternecineStrife.ServerSide.Model.Social
         {
             get
             {
-                return Nobility.Kids + Merchantes.Kids + ArtistCraft.Kids + Clir.Kids + Freeman.Kids;
+                return Nobility.Kids + Merchantes.Kids + ArtistCraft.Kids + Clir.Kids + Freeman.Kids + Serfs.Kids;
             }
         }
+        public int People { get => Men + Femen; }
 
         /// <summary>
         /// Распределение доходов/убытков между сословиями.
@@ -41,21 +44,13 @@ namespace TheInternecineStrife.ServerSide.Model.Social
         /// TODO Возвращать должна список новых дивизий наемников.
         public void DistributionFunds(float golds)
         {
-            Distributor destributer = (Stratum stratum, float gold) =>
-            {
-                var amountNow = (int)(Math.Min(stratum.Man, Math.Floor(golds / stratum.Class.Expenses)));
-                var declassed = stratum.Man - amountNow;
-                gold -= amountNow * stratum.Class.Expenses;
-                stratum.Man = amountNow;
-                return new Tuple<int, float>(declassed, gold);
-            };
-            var nobily = destributer(Nobility, golds);
-            var merch = destributer(Merchantes, nobily.Item2);
-            var craft = destributer(ArtistCraft, merch.Item2);
-            var doctors = destributer(Clir, craft.Item2);
-            var frees = destributer(Freeman, doctors.Item2);
-            var serfs = destributer(Serfs, frees.Item2);
-            destributer(Slaves, serfs.Item2);
+            var nobily = DistributeIncomeBetweenStratums(Nobility, golds);
+            var merch = DistributeIncomeBetweenStratums(Merchantes, nobily.Item2);
+            var craft = DistributeIncomeBetweenStratums(ArtistCraft, merch.Item2);
+            var doctors = DistributeIncomeBetweenStratums(Clir, craft.Item2);
+            var frees = DistributeIncomeBetweenStratums(Freeman, doctors.Item2);
+            var serfs = DistributeIncomeBetweenStratums(Serfs, frees.Item2);
+            DistributeIncomeBetweenStratums(Slaves, serfs.Item2);
 
             // Деклассирование разорившихся элементов.
             var toFree = nobily.Item1 + merch.Item1 + craft.Item1 + doctors.Item1;
@@ -68,64 +63,62 @@ namespace TheInternecineStrife.ServerSide.Model.Social
             Slaves.Kids += Math.Max(serfs.Item1, Serfs.Kids - serfs.Item1);
         }
 
-        /// <summary>
-        /// Рспределение трупов по сословиям. Чем меньше доля сословия, тем меньше у них смертей.
-        /// </summary>
-        /// <param name="man">Сколько умерло мужчин.</param>
-        /// <param name="feman">Сколько умерло женщин.</param>
-        /// <param name="kids">Сколько умерло детей.</param>
         public void Death(int man, int feman, int kids)
         {
-            var all = Men;
-            float nobilyProportion = Nobility.Man / all;
-            float traderProportion = Merchantes.Man / all;
-            float atisanProportion = ArtistCraft.Man / all;
-            float clirProportion   = Clir.Man / all;
-            float freesProportion  = Freeman.Man / all;
-            float servesProportion = Serfs.Man / all;
-            float slavesProportion = Slaves.Man / all;
-
-            Nobility.Man = Math.Max(0, Nobility.Man - (int)(man * nobilyProportion));
-            Merchantes.Man = Math.Max(0, Merchantes.Man - (int)(man * traderProportion));
-            ArtistCraft.Man = Math.Max(0, ArtistCraft.Man - (int)(man * atisanProportion));
-            Clir.Man = Math.Max(0, Clir.Man - (int)(man * clirProportion));
-            Freeman.Man = Math.Max(0, Freeman.Man - (int)(man * freesProportion));
-            Serfs.Man = Math.Max(0, Serfs.Man - (int)(man * servesProportion));
-            Slaves.Man = Math.Max(0, Slaves.Man - (int)(man * slavesProportion));
-
-            all = Femen;
-            nobilyProportion = Nobility.Feman / all;
-            traderProportion = Merchantes.Feman / all;
-            atisanProportion = ArtistCraft.Feman / all;
-            clirProportion = Clir.Feman / all;
-            freesProportion = Freeman.Feman / all;
-            servesProportion = Serfs.Feman / all;
-            slavesProportion = Slaves.Feman / all;
-
-            Nobility.Feman = Math.Max(0, Nobility.Feman - (int)(feman * nobilyProportion));
-            Merchantes.Feman = Math.Max(0, Merchantes.Feman - (int)(feman * traderProportion));
-            ArtistCraft.Feman = Math.Max(0, ArtistCraft.Feman - (int)(feman * atisanProportion));
-            Clir.Feman = Math.Max(0, Clir.Feman - (int)(feman * clirProportion));
-            Freeman.Feman = Math.Max(0, Freeman.Feman - (int)(feman * freesProportion));
-            Serfs.Feman = Math.Max(0, Serfs.Feman - (int)(feman * servesProportion));
-            Slaves.Feman = Math.Max(0, Slaves.Feman - (int)(feman * slavesProportion));
-
-            all = kids;
-            nobilyProportion = Nobility.Kids / all;
-            traderProportion = Merchantes.Kids / all;
-            atisanProportion = ArtistCraft.Kids / all;
-            clirProportion = Clir.Kids / all;
-            freesProportion = Freeman.Kids / all;
-            servesProportion = Serfs.Kids / all;
-            slavesProportion = Slaves.Kids / all;
-
-            Nobility.Kids = Math.Max(0, Nobility.Kids - (int)(kids * nobilyProportion));
-            Merchantes.Kids = Math.Max(0, Merchantes.Kids - (int)(kids * traderProportion));
-            ArtistCraft.Kids = Math.Max(0, ArtistCraft.Kids - (int)(kids * atisanProportion));
-            Clir.Kids = Math.Max(0, Clir.Kids - (int)(kids * clirProportion));
-            Freeman.Kids = Math.Max(0, Freeman.Kids - (int)(kids * freesProportion));
-            Serfs.Kids = Math.Max(0, Serfs.Kids - (int)(kids * servesProportion));
-            Slaves.Kids = Math.Max(0, Slaves.Kids - (int)(kids * slavesProportion));
+            CountAlivedMan(man);
+            CountAlivedWoman(feman);
+            CountAlivedKids(kids);
         }
+
+        private void CountAlivedMan(int lost)
+        {
+            var all = Men;
+            Nobility.Man = CountAlived(Nobility.Man, all, lost);
+            Clir.Man = CountAlived(Clir.Man, all, lost);
+            Merchantes.Man = CountAlived(Merchantes.Man, all, lost);
+            ArtistCraft.Man = CountAlived(ArtistCraft.Man, all, lost);
+            Freeman.Man = CountAlived(Freeman.Man, all, lost);
+            Serfs.Man = CountAlived(Serfs.Man, all, lost);
+            Slaves.Man = CountAlived(Slaves.Man, all, lost);
+        }
+
+        private void CountAlivedWoman(int lost)
+        {
+            var all = Femen;
+            Nobility.Feman = CountAlived(Nobility.Feman, all, lost);
+            Clir.Feman = CountAlived(Clir.Feman, all, lost);
+            Merchantes.Feman = CountAlived(Merchantes.Feman, all, lost);
+            ArtistCraft.Feman = CountAlived(ArtistCraft.Feman, all, lost);
+            Freeman.Feman = CountAlived(Freeman.Feman, all, lost);
+            Serfs.Feman = CountAlived(Serfs.Feman, all, lost);
+            Slaves.Feman = CountAlived(Slaves.Feman, all, lost);
+        }
+
+        private void CountAlivedKids(int lost)
+        {
+            var all = Childrens;
+            Nobility.Kids = CountAlived(Nobility.Kids, all, lost);
+            Merchantes.Kids = CountAlived(Merchantes.Kids, all, lost);
+            ArtistCraft.Kids = CountAlived(ArtistCraft.Kids, all, lost);
+            Clir.Kids = CountAlived(Clir.Kids, all, lost);
+            Freeman.Kids = CountAlived(Freeman.Kids, all, lost);
+            Serfs.Kids = CountAlived(Serfs.Kids, all, lost);
+            Slaves.Kids = CountAlived(Slaves.Kids, all, lost);
+        }
+
+        private int CountAlived(int stratumPeople, int allPeople, int dieds)
+        {
+            float proportion = stratumPeople / allPeople;
+            return Math.Max(0, Nobility.Man - (int)(dieds * proportion));
+        }
+
+        private readonly Distributor DistributeIncomeBetweenStratums = (Stratum stratum, float gold) =>
+        {
+            var amountNow = (int)(Math.Min(stratum.Man, Math.Floor(gold / stratum.Class.Expenses)));
+            var declassed = stratum.Man - amountNow;
+            gold -= amountNow * stratum.Class.Expenses;
+            stratum.Man = amountNow;
+            return new Tuple<int, float>(declassed, gold);
+        };
     }
 }
