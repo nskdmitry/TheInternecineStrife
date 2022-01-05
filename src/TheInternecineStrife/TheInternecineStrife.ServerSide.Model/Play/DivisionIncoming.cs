777ce3c82e4781999db2a03c3b12f25372fb7ba8
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-05 18:41:12 +0700

    feature(Game playing) New class DivisionIncoming.
    DivisionIncoming is status "division on marsh to army". Use it for collecting armies from divisions by times.

---
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Play/DivisionIncoming.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheInternecineStrife.ServerSide.Model.War;
+
+namespace TheInternecineStrife.ServerSide.Model.Play
+{
+    public enum ComingTo { Camp, MercenaryPost, TownGarrison, Castle };
+
+    public class DivisionIncoming
+    {
+        public Division Division { get; set; }
+        public Cell From { get; set; }
+        public Cell To { get; set; }
+        public int LeftTime { get; set; }
+        public int PathLength { get; set; }
+        public ComingTo Target { get; set; }
+        public bool Invited { get; set; }
+    }
+}
