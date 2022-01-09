fb85e171fb015344438495099a62239586caf270
nskdmitry <dmitry.m-ko2014@yandex.ru>
2022-01-09 21:08:08 +0700

    refactor(economic) New Treasury subclass is FinanceCenter.
    FinanceCenter is group of treasures and productions what work with more then one cell/object.

---
+++ src/TheInternecineStrife/TheInternecineStrife.ServerSide.Model/Economic/FinanceCenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheInternecineStrife.ServerSide.Model.Protocol;
+using TheInternecineStrife.ServerSide.Model.Social;
+using TheInternecineStrife.ServerSide.Model.World;
+
+namespace TheInternecineStrife.ServerSide.Model.Economic
+{
+    public class FinanceCenter : Treasury
+    {
+        public Map World { get; protected set; }
+        public Stratum Workers { get; protected set; }
+        public IEconomicable ServicesInstance { get; protected set; }
+
+        public FinanceCenter(Map linkForWorld, IEconomicable of, Stratum workers)
+        {
+            World = linkForWorld;
+            Workers = workers;
+            ServicesInstance = of;
+        }
+
+        public override float Product(float instruments)
+        {
+            return base.Product(instruments);
+        }
+    }
+}
